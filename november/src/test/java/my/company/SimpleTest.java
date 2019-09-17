4eec83cdcc165d4907c83d7a6199a2e74792dcf7
Teodor5 <pastushenko.bohdan@gmail.com>
2019-09-17 14:51:38 +0300

    initial structure

---
+++ november/src/test/java/my/company/SimpleTest.java
@@ -0,0 +1,17 @@
+package my.company;
+
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+/**
+ * @author baev (Dmitry Baev)
+ */
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() {
+        System.out.println("task with selenium");
+    }
+}
+
